e3415a3b6d13916ecdbe39407bec656f8608f4dc
isdemx <128055001+isdemx@users.noreply.github.com>
2024-12-09 17:24:41 +0200

    Update index.js - tmp add token

--- admin/src/components/Configuration/index.js
+++ admin/src/components/Configuration/index.js
@@ -40,7 +40,7 @@ import Banner from './banner';
 import pluginPkg from '../../../../package.json';
 import WarningIcon from './warningIcon';
 
-const apiToken = process.env.STRAPI_ADMIN_API_TOKEN;
+const apiToken = '6bf7a9cc1e60ab5c27309df7a11c1c7b913f20db60438c61143fd25eb604c88e445642c5cfb900960531bd63321add06a54ea7ffbe98f00d1657717318906afa561fa88418af6ec4780abeb798ff2b4aad750e442f4d57751a3c226dfe7848aca439fbebe4122554ad54625cf227bf08a3591f58ee16e3e05a0ae971cfb499c2';// process.env.STRAPI_ADMIN_API_TOKEN;
 
 const Configuration = () => {
   const [stripeConfiguration, setStripeConfiguration] = useState({
